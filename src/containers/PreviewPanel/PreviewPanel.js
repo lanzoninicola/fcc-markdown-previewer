0c0eafb2b9d87af397ba025c9b913fe59f1650ef
Nicola <lanzoni.nicola@gmail.com>
2020-07-29 15:54:37 +0200

    added style selection in preview

--- src/containers/PreviewPanel/PreviewPanel.js
+++ src/containers/PreviewPanel/PreviewPanel.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
 import './PreviewPanel.css'
-import './PreviewRenderGithub.css'
+// import './PreviewRenderDefault.css'
+// import './PreviewRenderGithub.css'
 import marked from 'marked'
 
+const DEFAULT = 'Default';
+const GITHUB = 'Github Style'
+
 class PreviewPanel extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            previewRenderStyles: [DEFAULT, GITHUB],
+            previewRenderClassName: 'preview-render-default'
+        }
+    }
+
+    componentDidMount() {
+        require('./PreviewRenderDefault.css')
+    }
 
     createMarkup = (text) => {
         marked.setOptions({
@@ -23,16 +39,46 @@ class PreviewPanel extends Component {
         return { __html: markup };
     }
 
+    handleStyleChange = (e) => {
+        let previewRenderClassNameChoosed = '';
+
+        switch (e.target.value) {
+            case DEFAULT:
+                require('./PreviewRenderDefault.css')
+                previewRenderClassNameChoosed = "preview-render-default"
+                break;
+            case GITHUB:
+                require('./PreviewRenderGithub.css')
+                previewRenderClassNameChoosed = "preview-render-github"
+                break;
+            default:
+                break;
+        }
+
+        this.setState({ previewRenderClassName: previewRenderClassNameChoosed })
+    }
+
     render() {
 
         const { rawText } = this.props;
+        const { previewRenderStyles, previewRenderClassName } = this.state;
+
+        const stylesOptions = previewRenderStyles.map((previewRenderStyle, i) => {
+            return <option key={i}>{previewRenderStyle}</option>
+        })
 
         return (
             <div id="preview-panel">
                 <div id="preview-panel-header">
                     <h3>PREVIEW</h3>
+                    <div id="preview-panel-header-select">
+                        <label htmlFor="markup-version" id="preview-panel-header-select-label">Style:</label>
+                        <select value={this.state.style} onChange={this.handleStyleChange}>
+                            {stylesOptions}
+                        </select>
+                    </div>
                 </div>
-                <div dangerouslySetInnerHTML={this.createMarkup(rawText)} id="preview-render" className="preview-render-github" >
+                <div dangerouslySetInnerHTML={this.createMarkup(rawText)} id="preview-render" className={previewRenderClassName} >
                 </div>
             </div>
         )
